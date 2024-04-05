78ae2bbfa5fad26f9790682e3d6d39631a390895
2310019 <bob.huang@iisigroup.com>
2024-04-06 01:49:56 +0800

    version 0

---
+++ BobMall/src/main/java/tw/com/mall/BobMallApplication.java
@@ -0,0 +1,13 @@
+package tw.com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobMallApplication.class, args);
+	}
+
+}
---
+++ BobMall/src/test/java/tw/com/mall/BobMallApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
